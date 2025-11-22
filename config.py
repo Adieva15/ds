7625a83c185727fd489313be6452ccc5fc2bd04d
diana-0-0 <dianank2005@mail.ru>
2025-11-22 17:00:50 +0300

    ujhdb

--- config.py
+++ config.py
@@ -1,9 +1,6 @@
 # === НАСТРОЙКИ ===
 import os
 
-# Токены
-AI_TOKEN = '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'
-BOT_TOKEN = '7971545933:AAHJpI7CzpfvlYVF5y9liUqx4RyjDMJbmPA'
 
 # Настройки OpenAI
 OPENAI_BASE_URL = "https://openrouter.ai/api/v1"
