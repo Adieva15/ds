9eab12b39fa2e030da4ec38a54b5b84e6b66391a
Adieva15 <adievadine@gmail.com>
2025-12-01 15:44:03 +0300

    Update IUser.ts

--- src/client/src/interfaces/IUser.ts
+++ src/client/src/interfaces/IUser.ts
@@ -1,5 +1,13 @@
 export interface IUser {
     id: number;
+    
     created_at: Date;
     name: string
+    /*
+    telegram_id: number;
+    username: string | null;
+    full_name: string;
+    workouts_count: number;
+    goal: string | null;
+    created_at: Date;*/
 }
