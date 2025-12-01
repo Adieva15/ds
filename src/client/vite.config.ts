47a63710bf3acde82d54e74277f557b023df1c66
Adieva15 <adievadine@gmail.com>
2025-12-01 15:46:29 +0300

    Update vite.config.ts

--- src/client/vite.config.ts
+++ src/client/vite.config.ts
@@ -7,5 +7,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {allowedHosts: ['https://palladic-cheree-noncommendably.ngrok-free.dev']}
+  server: {allowedHosts: ['palladic-cheree-noncommendably.ngrok-free.dev']} // del hhttps://
 })
