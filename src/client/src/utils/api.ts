6093f2bc75fac4a0e94cf102691ae51e4f5adf4a
Adieva15 <adievadine@gmail.com>
2025-12-01 15:43:08 +0300

    Update api.ts

--- src/client/src/utils/api.ts
+++ src/client/src/utils/api.ts
@@ -8,13 +8,15 @@ import {initData } from "@tma.js/sdk"
 const BASE_API_URL = import.meta.env.VITE_BASE_API_URL;
 
 const request = async (endpoint: string , method:string ="GET", data?:any) => {
+    #const initData = (window as any).Telegram?.WebApp?.initData;
+    
     const response = await axios.request({
         url: '${BASE_API_URL}/api/${endpoint}',
         method:method,
         headers:{
             initData: '${initData.raw()}',
             Accept:"application/json",
-            "Content-Type":"aplication/json"
+            "Content-Type":"application/json"
         },
         data : data ? JSON.stringify(data) : undefined
     })
